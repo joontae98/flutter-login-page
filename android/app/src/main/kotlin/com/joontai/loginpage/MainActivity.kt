a44b15243b52e335d6ddbae817b4867325773365
joontai98 <joontae98@gmail.com>
2020-08-23 09:22:51 +0900

    First commit

---
+++ android/app/src/main/kotlin/com/joontai/loginpage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.joontai.loginpage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login_app_home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(color: Color(0xff000031),),
+          Column(
+            children: <Widget>[
+              Container(width: 200,height: 200,color: Colors.deepPurpleAccent,),
+              Container(width: 200,height: 200,color: Color(0xff02F6D3),),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
